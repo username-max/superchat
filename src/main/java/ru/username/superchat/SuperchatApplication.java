3109de352adc8179915ccef55b1a32bdefdf013d
username-max <username.1101@mail.ru>
2020-12-22 04:54:03 +0300

    Start

---
+++ src/main/java/ru/username/superchat/SuperchatApplication.java
@@ -0,0 +1,13 @@
+package ru.username.superchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/username/superchat/SuperchatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.username.superchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
